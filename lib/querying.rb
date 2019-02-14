2b464c51c5d92fea5b08e7cf02614b2438c2d1f8
Mitchel Parish <mitchelparish@gmail.com>
2019-02-14 04:47:39 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,5 +37,11 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(*)
+  FROM characters
+  JOIN character_books
+  ON characters.id = character_books.characters_id
+  JOIN books ON character_books.book_id = book.id
+  GROUP BY characters.id
+  ORDER BY COUNT(*) DESC, characters.name;"
 end
