2d475db1c519254b705682f1d280dd2c72c341a9
Mitchel Parish <mitchelparish@gmail.com>
2019-02-14 04:43:09 +0000

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   JOIN series
   ON books.series_id = series.id
-  Where series.id = 1;"
+  WHERE series.id = 1;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
@@ -27,7 +27,13 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+  FROM series
+  JOIN characters
+  ON characters.series_id = series.id
+  WHERE characters.species = 'human'
+  GROUP BY series.title
+  ORDER BY COUNT(characters.species) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
